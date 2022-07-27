80873b8530bea45eeb06edcf108cdd00c264b7c5
Tom60340 <r.t.k60@hotmail.fr>
2022-07-27 15:26:42 +0200

    content added to index page

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+/*snackbar */
 function snackPop() {
   // Get the snackbar DIV
-  var x = document.getElementById("snackbar");
+  let x = document.getElementById("snackbar");
 
   // Add the "show" class to DIV
   x.className = "show";
 
   // After 3 seconds, remove the show class from DIV
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  setTimeout(function() { x.className = x.className.replace("show", "");}, 3000);
 
   // redirect to index.html after 6s
-  let timer = setTimeout(function() {
-    window.location='./index.html'
-  }, 2500);
-} 
+  setTimeout(function() {window.location='./index.html'}, 2500);} 
+
+/* backtotop button */
+//Get the button:
+let backToTop = document.getElementById("backToTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+}
