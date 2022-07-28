64dca0c208c8a877dad16d7b0d21358121e52187
Tom60340 <r.t.k60@hotmail.fr>
2022-07-28 17:48:10 +0200

    added burger menu done

--- script.js
+++ script.js
@@ -35,3 +35,21 @@ document.getElementById('form').addEventListener('submit',function(e){
   snackPop();
 })
 }
+
+/* topnav */
+let sidenav = document.getElementById("mySidenav");
+let openBtn = document.getElementById("openBtn");
+let closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
